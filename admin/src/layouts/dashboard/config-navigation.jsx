cb67b771a373f2337aed817e7775e856bc946d36
hptprobook <hptprobook@gmail.com>
2024-05-27 21:10:56 +0700

    ticket page, axios config

--- admin/src/layouts/dashboard/config-navigation.jsx
+++ admin/src/layouts/dashboard/config-navigation.jsx
@@ -33,6 +33,11 @@ const navConfig = [
     icon: icon('ic_lock'),
   },
   {
+    title: 'vé',
+    path: '/tickets',
+    icon: icon('ic_lock'),
+  },
+  {
     title: 'Not found',
     path: '/404',
     icon: icon('ic_disabled'),
