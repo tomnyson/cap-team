af816dada2efb25300c3642282c3a96afba2d83d
hptprobook <hptprobook@gmail.com>
2024-05-27 23:16:57 +0700

    ticket

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
 import checker from 'vite-plugin-checker';
+import react from '@vitejs/plugin-react-swc';
 
 // ----------------------------------------------------------------------
 
